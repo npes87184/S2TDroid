8226f6d1e43ddb31344300c03febe5f17328bd98
npes87184 <npes87184@gmail.com>
2015-08-14 23:45:21 +0800

    delete floatActivity. Instead, use dialog

--- app/src/main/java/com/npes87184/s2tdroid/BubbleService.java
+++ app/src/main/java/com/npes87184/s2tdroid/BubbleService.java
@@ -1,10 +1,18 @@
 package com.npes87184.s2tdroid;
 
+import android.app.AlertDialog;
 import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.premnirmal.Magnet.IconCallback;
@@ -17,7 +25,8 @@ public class BubbleService extends Service implements IconCallback {
 
     private static final String TAG = "Magnet";
     private Magnet mMagnet;
-    private boolean isOpne = false;
+    private static final String KEY_MODE = "mode";
+    private SharedPreferences prefs;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,6 +36,7 @@ public class BubbleService extends Service implements IconCallback {
     @Override
     public void onCreate() {
         super.onCreate();
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
         ImageView iconView = new ImageView(this);
         iconView.setImageResource(R.drawable.ic_launcher);
         mMagnet = new Magnet.Builder(this)
@@ -48,15 +58,33 @@ public class BubbleService extends Service implements IconCallback {
 
     @Override
     public void onMove(float x, float y) {
-        Log.i(TAG, "onMove(" + x + "," + y + ")");
+
     }
 
     @Override
     public void onIconClick(View icon, float iconXPose, float iconYPose) {
         Log.i(TAG, "onIconClick(..)");
-        Intent window = new Intent(this, com.npes87184.s2tdroid.BubbleActivity.class);
-        window.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(window);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("S2TDroid");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String out = input.getText().toString();
+                out = prefs.getString(KEY_MODE, "s2t").equals("s2t") ? Analysis.StoT(out):Analysis.TtoS(out);
+                copyToClipboard(out);
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        alert.show();
     }
 
     @Override
@@ -64,5 +92,9 @@ public class BubbleService extends Service implements IconCallback {
         Log.i(TAG, "onIconDestroyed()");
         stopSelf();
     }
-
+    private void copyToClipboard(String str) {
+        android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        android.content.ClipData clip = android.content.ClipData.newPlainText("text label", str);
+        clipboard.setPrimaryClip(clip);
+    }
 }
