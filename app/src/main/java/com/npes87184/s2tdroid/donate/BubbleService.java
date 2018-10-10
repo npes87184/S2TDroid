dfc4e78bbf932d17805602464903691fe4fe9493
yuchenlin <npes87184@gmail.com>
2018-10-10 14:17:05 +0800

    replace Magnet with FloatingView

--- app/src/main/java/com/npes87184/s2tdroid/donate/BubbleService.java
+++ app/src/main/java/com/npes87184/s2tdroid/donate/BubbleService.java
@@ -3,39 +3,123 @@ package com.npes87184.s2tdroid.donate;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AlertDialog;
-import android.view.ContextThemeWrapper;
+import android.support.v7.view.ContextThemeWrapper;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.facebook.rebound.SpringConfig;
 import com.npes87184.s2tdroid.donate.model.Analysis;
 import com.npes87184.s2tdroid.donate.model.KeyCollection;
-import com.premnirmal.Magnet.IconCallback;
-import com.premnirmal.Magnet.Magnet;
+
+import jp.co.recruit_lifestyle.android.floatingview.FloatingViewListener;
+import jp.co.recruit_lifestyle.android.floatingview.FloatingViewManager;
 
 /**
  * Created by npes87184 on 2015/8/14.
  */
-public class BubbleService extends Service implements IconCallback {
+public class BubbleService extends Service implements FloatingViewListener {
 
-    private Magnet mMagnet;
+    private static final int NOTIFICATION_ID = 801105;
+    private FloatingViewManager mFloatingViewManager;
     private SharedPreferences prefs;
-    private float scale;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mFloatingViewManager != null) {
+            return START_STICKY;
+        }
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final DisplayMetrics metrics = new DisplayMetrics();
+        final WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+        final LayoutInflater inflater = LayoutInflater.from(this);
+        final ImageView iconView = (ImageView) inflater.inflate(R.layout.floating_bubble, null, false);
+        iconView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getApplicationContext(), R.style.AppCompatAlertDialogStyle));
+                String mode;
+                if(prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "0").equals("0")) {
+                    mode = getString(R.string.auto_detect);
+                } else {
+                    mode = prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "s2t").equals("s2t")?getString(R.string.s2t):getString(R.string.t2s);
+                }
+                builder.setTitle("S2TDroid"+'-'+ mode);
+
+                // Set up the input
+                final EditText input = new EditText(getApplicationContext());
+                input.setHint(getString(R.string.dialogHint));
+                input.setHintTextColor(Color.parseColor("#757575"));
+                input.setTextColor(Color.parseColor("#000000"));
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                builder.setView(input);
+                // Set up the buttons
+                builder.setPositiveButton(getString(R.string.copy), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String out = input.getText().toString();
+                        if(prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "0").equals("0")) {
+                            out = Analysis.isTraditional(out)>=0 ? Analysis.TtoS(out):Analysis.StoT(out);
+                        } else {
+                            out = prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "s2t").equals("s2t") ? Analysis.StoT(out):Analysis.TtoS(out);
+                        }
+                        copyToClipboard(out);
+                    }
+                });
+                AlertDialog alert = builder.create();
+                // in Lollipop the dialog will be covered by input...
+                int size = (int)(150 * getResources().getDisplayMetrics().density);
+                alert.getWindow().getAttributes().y = -1*size;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    alert.getWindow().setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
+                } else {
+                    alert.getWindow().setType(WindowManager.LayoutParams.TYPE_PHONE);
+                }
+                alert.show();
+            }
+        });
+
+        mFloatingViewManager = new FloatingViewManager(this, this);
+        mFloatingViewManager.setFixedTrashIconImage(resize(R.drawable.ic_trash_fixed, 96));
+        mFloatingViewManager.setActionTrashIconImage(resize(R.drawable.ic_trash_action, 200));
+        final FloatingViewManager.Options options = new FloatingViewManager.Options();
+        options.floatingViewX = (int) convertDpToPixel(512, this);
+        options.floatingViewY = (int) convertDpToPixel(512, this);
+        options.floatingViewHeight = (int) convertDpToPixel(96, this);
+        options.floatingViewWidth = (int) convertDpToPixel(96, this);
+        options.overMargin = (int) (16 * metrics.density);
+        mFloatingViewManager.addViewToWindow(iconView, options);
+
+        startForeground(NOTIFICATION_ID, createNotification(this));
+
+        return START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public void onDestroy() {
+        destroy();
+        super.onDestroy();
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -43,119 +127,46 @@ public class BubbleService extends Service implements IconCallback {
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        SpringConfig springConfig = SpringConfig.fromBouncinessAndSpeed(10, 50);
-
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this, createNotificationChannel())
-                        .setSmallIcon(R.drawable.notification)
-                        .setContentTitle(getString(R.string.app_name))
-                        .setContentText(getString(R.string.notification))
-                        .setPriority(NotificationCompat.PRIORITY_MIN);
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, MainActivity.class);
-
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(MainActivity.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        startForeground(1, mBuilder.build());
-
-        scale = getResources().getDisplayMetrics().density;
-        int size = (int) (60 * scale);
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        ImageView iconView = new ImageView(this);
-        iconView.setImageResource(R.drawable.telegram);
-        iconView.setAdjustViewBounds(true);
-        iconView.setMaxHeight(size);
-        iconView.setMaxWidth(size);
-        iconView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        mMagnet = Magnet.newBuilder(this)
-                .setIconView(iconView)
-                .setIconCallback(this)
-                .setShouldShowRemoveView(true)
-                .setShouldStickToWall(true)
-                .setRemoveIconShouldBeResponsive(true)
-                .setInitialPosition(300, 400)
-                .setHideFactor(0.2f)
-                .withSpringConfig(springConfig)
-                .build();
-        mMagnet.show();
-    }
-
-    @Override
-    public void onFlingAway() {
-        mMagnet.destroy();
+    public void onFinishFloatingView() {
         stopSelf();
     }
 
     @Override
-    public void onMove(float x, float y) {
-        mMagnet.goToWall();
+    public void onTouchFinished(boolean isFinishing, int x, int y) {
+
     }
 
-    @Override
-    public void onIconClick(View icon, float iconXPose, float iconYPose) {
-
-        mMagnet.goToWall();
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AppCompatAlertDialogStyle));
-        String mode;
-        if(prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "0").equals("0")) {
-            mode = getString(R.string.auto_detect);
-        } else {
-            mode = prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "s2t").equals("s2t")?getString(R.string.s2t):getString(R.string.t2s);
+    private void destroy() {
+        if (mFloatingViewManager != null) {
+            mFloatingViewManager.removeAllViewToWindow();
+            mFloatingViewManager = null;
         }
-        builder.setTitle("S2TDroid"+'-'+ mode);
-
-        // Set up the input
-        final EditText input = new EditText(this);
-        input.setHint(getString(R.string.dialogHint));
-        input.setHintTextColor(Color.parseColor("#757575"));
-        input.setTextColor(Color.parseColor("#000000"));
-        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        builder.setView(input);
-        // Set up the buttons
-        builder.setPositiveButton(getString(R.string.copy), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String out = input.getText().toString();
-                if(prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "0").equals("0")) {
-                    out = Analysis.isTraditional(out)>=0 ? Analysis.TtoS(out):Analysis.StoT(out);
-                } else {
-                    out = prefs.getString(KeyCollection.KEY_BUBBLE_MODE, "s2t").equals("s2t") ? Analysis.StoT(out):Analysis.TtoS(out);
-                }
-                copyToClipboard(out);
-                mMagnet.goToWall();
-            }
-        });
-        AlertDialog alert = builder.create();
-        // in Lollipop the dialog will be covered by input...
-        int size = (int)(150 * scale);
-        alert.getWindow().getAttributes().y = -1*size;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            alert.getWindow().setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
-        } else {
-            alert.getWindow().setType(WindowManager.LayoutParams.TYPE_PHONE);
-        }
-        alert.show();
     }
 
-    @Override
-    public void onIconDestroyed() {
+    public static float convertDpToPixel(float dp, Context context){
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return px;
+    }
 
+    private Drawable resize(int imageId, int radius) {
+        Bitmap b = BitmapFactory.decodeResource(getResources(), imageId);
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, radius, radius, false);
+        return new BitmapDrawable(getResources(), bitmapResized);
+    }
+
+    private Notification createNotification(Context context) {
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, createNotificationChannel());
+        builder.setWhen(System.currentTimeMillis());
+        builder.setSmallIcon(R.drawable.notification);
+        builder.setContentTitle(context.getString(R.string.app_name));
+        builder.setContentText(context.getString(R.string.notification));
+        builder.setOngoing(true);
+        builder.setPriority(NotificationCompat.PRIORITY_MIN);
+        builder.setCategory(NotificationCompat.CATEGORY_SERVICE);
+
+        return builder.build();
     }
 
     private void copyToClipboard(String str) {
