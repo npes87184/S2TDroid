284ca495a24c6ef7844f81b083b4b55aa778c0c6
Yu-Chen Lin <npes87184@gmail.com>
2020-08-29 11:19:10 +0800

    To androidx

--- app/src/main/java/com/npes87184/s2tdroid/donate/model/FileUtil.java
+++ app/src/main/java/com/npes87184/s2tdroid/donate/model/FileUtil.java
@@ -6,9 +6,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.storage.StorageManager;
 import android.provider.DocumentsContract;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.provider.DocumentFile;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.documentfile.provider.DocumentFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
--- app/src/main/java/com/npes87184/s2tdroid/donate/model/ListAdapter.java
+++ app/src/main/java/com/npes87184/s2tdroid/donate/model/ListAdapter.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Build;
-import android.support.annotation.DrawableRes;
+import androidx.annotation.DrawableRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
