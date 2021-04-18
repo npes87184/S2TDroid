413e005c4fdf7881880ae4fe626b67f62685bdff
Yu-Chen Lin <npes87184@gmail.com>
2021-04-18 11:50:32 +0800

    Re:Zero
    Signed-off-by: Yu-Chen Lin <npes87184@gmail.com>

---
+++ app/src/main/java/com/npes87184/s2tdroid/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.npes87184.s2tdroid.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
