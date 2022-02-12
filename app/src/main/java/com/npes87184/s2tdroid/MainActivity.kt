2664d9da1960b80ded92bbc616f2218a01a060e9
Yu-Chen Lin <npes87184@gmail.com>
2022-02-12 15:03:11 +0800

    Support transform
    Signed-off-by: Yu-Chen Lin <npes87184@gmail.com>

--- app/src/main/java/com/npes87184/s2tdroid/MainActivity.kt
+++ app/src/main/java/com/npes87184/s2tdroid/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.npes87184.s2tdroid
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private val permissionsList: MutableList<String> = ArrayList()
+    private val requestCodeAskPermission = 124
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,10 +38,100 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val permissionsNeeded: MutableList<String> = ArrayList()
+
+        if (!addPermission(
+                permissionsList,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) permissionsNeeded.add("READ_EXTERNAL_STORAGE")
+        if (!addPermission(
+                permissionsList,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) permissionsNeeded.add("WRITE_EXTERNAL_STORAGE")
+
+        if (permissionsList.size > 0) {
+            showUpPermissionDialog()
+        }
+    }
+
+    private fun showUpPermissionDialog() {
+        val alertDialog: AlertDialog = this.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setTitle(getString(R.string.app_name))
+                setMessage(getString(R.string.file_permission))
+                setNegativeButton(android.R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        finish()
+                    })
+                setPositiveButton(android.R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            permissionsList.toTypedArray(),
+                            requestCodeAskPermission
+                        )
+                    })
+            }
+
+            // Create the AlertDialog
+            builder.create()
+        }
+        alertDialog.show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun addPermission(permissionsList: MutableList<String>, permission: String): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsList.add(permission)
+            // Check for Rationale Option
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    permission
+                )
+            ) return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            requestCodeAskPermission -> {
+                val perms: MutableMap<String, Int> = HashMap()
+                // Initial
+                perms[Manifest.permission.READ_EXTERNAL_STORAGE] = PackageManager.PERMISSION_GRANTED
+                perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] =
+                    PackageManager.PERMISSION_GRANTED
+
+                // Fill with results
+                var i = 0
+                while (i < permissions.size) {
+                    perms[permissions[i]] = grantResults[i]
+                    i++
+                }
+                // Check for ACCESS_FINE_LOCATION
+                if (perms[Manifest.permission.READ_EXTERNAL_STORAGE] != PackageManager.PERMISSION_GRANTED
+                    || perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] != PackageManager.PERMISSION_GRANTED
+                ) {
+                    showUpPermissionDialog()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 }
--- app/src/main/java/com/npes87184/s2tdroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/npes87184/s2tdroid/ui/home/HomeFragment.kt
@@ -1,18 +1,37 @@
 package com.npes87184.s2tdroid.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.anggrayudi.storage.SimpleStorageHelper
+import com.anggrayudi.storage.extension.closeStreamQuietly
+import com.anggrayudi.storage.file.getBasePath
+import com.anggrayudi.storage.file.openInputStream
+import com.anggrayudi.storage.file.openOutputStream
 import com.npes87184.s2tdroid.R
+import com.npes87184.s2tdroid.libs.Transformer.toTraditional
+import org.mozilla.universalchardet.UniversalDetector
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private val storageHelper = SimpleStorageHelper(this)
+    private var sourceFile: DocumentFile? = null
+    private var targetFolder: DocumentFile? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +41,82 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.select_file_text)
+        val filesTextView: TextView = root.findViewById(R.id.select_file_text)
+        val outputFolderTestView: TextView = root.findViewById(R.id.output_folder_text)
+        val startBtn: Button = root.findViewById<Button>(R.id.start_button)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            filesTextView.text = it
         })
+        val filesSelectionCard = root.findViewById<CardView>(R.id.select_file_card)
+        val targetFolderSelectionCard = root.findViewById<CardView>(R.id.select_target_folder_card)
+
+        filesSelectionCard.setOnClickListener {
+            storageHelper.openFilePicker()
+        }
+
+        targetFolderSelectionCard.setOnClickListener {
+            storageHelper.openFolderPicker()
+        }
+
+        storageHelper.onFolderSelected = { requestCode, folder ->
+            outputFolderTestView.text = folder.getBasePath(container!!.context)
+            targetFolder = folder
+        }
+        storageHelper.onFileSelected = { requestCode, files ->
+            if (files.isNotEmpty()) {
+                filesTextView.text = files[0].getBasePath(container!!.context)
+                sourceFile = files[0]
+            }
+        }
+
+        startBtn.setOnClickListener {
+            startTransform(container!!.context)
+        }
+
         return root
+    }
+
+    private fun startTransform(context: Context) {
+        if (sourceFile == null || targetFolder == null) {
+            return
+        }
+        val encode = getEncode(context, sourceFile!!)
+        val inputStream = sourceFile!!.openInputStream(context)
+        val targetFolderUri = DocumentFile.fromTreeUri(context, targetFolder!!.uri)
+        val destFile = targetFolderUri!!.createFile("text/plain", "123.txt")
+        val destStream = destFile!!.openOutputStream(context, false)
+        val destStreamWriter = OutputStreamWriter(destStream)
+        val destBufferedWriter = BufferedWriter(destStreamWriter)
+
+        BufferedReader(InputStreamReader(inputStream, encode)).forEachLine {
+            destBufferedWriter.write(toTraditional(it))
+            destBufferedWriter.newLine()
+        }
+
+        destBufferedWriter.close()
+        destStreamWriter.close()
+        destStream.closeStreamQuietly()
+        inputStream.closeStreamQuietly()
+    }
+
+    private fun getEncode(context: Context, file: DocumentFile): String {
+        val inputStream = file.openInputStream(context)
+        val buf = ByteArray(4096)
+        val detector = UniversalDetector(null)
+        var nread: Int
+        var encodeString: String
+
+        while (inputStream!!.read(buf).also { nread = it } > 0 && !detector.isDone) {
+            detector.handleData(buf, 0, nread)
+        }
+        detector.dataEnd()
+
+        encodeString = detector.detectedCharset
+        if (encodeString == null) {
+            encodeString = "Unicode"
+        }
+        detector.reset()
+        inputStream.closeStreamQuietly()
+        return encodeString
     }
 }
